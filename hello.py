8cf09f2af1562d8c48b3687ea9400541464d2f54
Vanush Vaswani <vanush@gmail.com>
2019-07-07 12:45:37 +0000

    working web server

--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
+import numpy as np
+import time
+import os
+import glob
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/bodhi')
+def bodhi(directory):
+    img_list = glob.glob(os.path.join(directory, '*.jpg'))
+    images = sorted(img_list, key=os.path.getmtime)
+
+    # serve statistics to template
+    # seconds to minutes
+    intervals = np.diff(np.array(list(map(os.path.getmtime, images)))) / 60
+    average_interval = np.mean(intervals)
+    largest_interval = np.max(intervals)
+
+
+    return render_template('base.html', num_images=len(images),
+                           first_img_date=time.ctime(os.path.getmtime(images[0])),
+                           last_img_date=time.ctime(os.path.getmtime(images[-1])),
+                           average_interval=average_interval,
+                           largest_interval=largest_interval)
--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,8 @@
+import argparse
 import glob
+import shutil
+import sys
+import tempfile
 import os.path
 import ffmpeg
 from joblib import Parallel, delayed
@@ -6,17 +10,31 @@ from tqdm import tqdm
 import os
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
+import gc
+import dateutil.parser
+from datetime import datetime, timedelta, timezone
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+NUM_DAYS = 30
+FRAMERATE = 24
+VIDEO_WIDTH = 600
 
-drive = GoogleDrive(gauth)
+def file_in_range(file_obj, num_hours):
+    file_date = dateutil.parser.parse(file_obj['modifiedDate'])
+    now = datetime.now(timezone.utc)
+    if (now - file_date) < timedelta(hours=num_hours):
+        return True
 
-def get_file(file_obj):
-    file_path = os.path.join('bodhi4', file_obj['title'])
+    return False
+
+def get_file(directory, file_obj):
+    file_path = os.path.join(directory, file_obj['title'])
     if os.path.isfile(file_path):
-        return
+        return file_path
+
     file_obj.GetContentFile(file_path)
+    del file_obj
+    gc.collect()
+    return file_path
 
 def get_folder_id(file_list, folder_name):
     for file1 in file_list:
@@ -25,20 +43,43 @@ def get_folder_id(file_list, folder_name):
             return folder_id
     return None
 
-def get_files_in_folder(folder_id):
-    file_list = drive.ListFile({'q': "'{0}' in parents and trashed=false".format(folder_id)}).GetList()
+def get_files_in_folder(drive, folder_id):
+    '''Get list of files in GDrive folder'''
+    file_list = drive.ListFile({'q': "'{0}' in parents and trashed=false".
+                               format(folder_id)}).GetList()
     return file_list
 
-# Auto-iterate through all files that matches this query
+def main(directory, output_file_name):
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
 
-file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-folder_id = get_folder_id(file_list, 'bodhi4')
-bodhi_files = get_files_in_folder(folder_id)
-#
-Parallel(n_jobs=16, prefer='threads')(delayed(get_file)(i) for i in tqdm(bodhi_files))
-stream = ffmpeg.input('bodhi4/*.jpg', pattern_type='glob', framerate=18)
-stream = ffmpeg.filter(stream, 'transpose', 1)
-stream = ffmpeg.vflip(stream)
-stream = ffmpeg.drawtext(stream, text="%{frame_num}", start_number='0', fontfile='OCRB Medium.ttf', fontcolor='white', borderw='1', fontsize='100', x=500, y=700, escape_text=False)
-stream = ffmpeg.output(stream, 'movie.mp4', crf=20)
-ffmpeg.run(stream, overwrite_output=True)
+    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    folder_id = get_folder_id(file_list, directory)
+    
+    timelapse_files = get_files_in_folder(drive, folder_id)
+    timelapse_files = [f for f in timelapse_files if file_in_range(f, 24*NUM_DAYS)]
+    output = Parallel(n_jobs=4, prefer='threads')(delayed(get_file)(directory, i)
+                                                  for i in tqdm(timelapse_files))
+    
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        for f in output:
+            shutil.copy(f, os.path.join(tmp_dir, os.path.basename(f)))
+        stream = ffmpeg.input('%s/*.jpg' % (tmp_dir), pattern_type='glob', framerate=FRAMERATE)
+        stream = ffmpeg.filter(stream, 'transpose', 1)
+        stream = ffmpeg.filter(stream, 'scale', VIDEO_WIDTH, -2)
+        stream = ffmpeg.vflip(stream)
+        stream = ffmpeg.output(stream, output_file_name, crf=23)
+        ffmpeg.run(stream, overwrite_output=True)
+
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory')
+    parser.add_argument('--output', required=True)
+    args = parser.parse_args()
+    sys.exit(main(args.directory, args.output))
+
+
